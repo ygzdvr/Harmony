c7adf29c2681199fc6d7e747d5d973ce3d45ac7d
Yagiz Devre <hd0216@princeton.edu>
2023-11-09 03:31:02 -0500

    some responsivity fixes

--- Harmony/screens/authentication/WelcomeView.js
+++ Harmony/screens/authentication/WelcomeView.js
@@ -2,90 +2,33 @@
 import {View, Text, TouchableOpacity, Image, Pressable} from 'react-native';
 import React from 'react';
 import COLORS from '../../constants/colors';
+import WelcomeStyles from '../../constants/styles/WelcomeStyles';
 
 import GradientText from '../../components/GradientText';
 const WelcomeView = ({navigation}) => {
   return (
-    <View
-      style={{
-        backgroundColor: COLORS.background,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <View
-        style={{
-          width: '100%',
-          alignItems: 'center',
-          marginBottom: 275,
-        }}>
+    <View style={WelcomeStyles.container}>
+      <View style={WelcomeStyles.logoContainer}>
         <Image
           source={require('../../assets/logo/logo.png')}
-          style={{width: 300, height: 300}}
+          style={WelcomeStyles.logo}
         />
       </View>
-      <View
-        style={{
-          position: 'absolute',
-          top: 350,
-          width: '100%',
-        }}>
-        <Text
-          style={{
-            color: COLORS.text,
-            fontSize: 45,
-            fontWeight: 'bold',
-            marginTop: 20,
-            paddingHorizontal: 10,
-          }}>
-          Welcome to
-        </Text>
-
-        <GradientText
-          style={{
-            fontSize: 45,
-            fontWeight: 'bold',
-            marginTop: 0,
-            paddingHorizontal: 10,
-          }}>
-          Harmony
-        </GradientText>
-        <Text
-          style={{
-            color: COLORS.text,
-            fontSize: 16,
-            marginTop: 20,
-            paddingHorizontal: 10,
-          }}>
+      <View style={WelcomeStyles.contentContainer}>
+        <Text style={WelcomeStyles.title}>Welcome to</Text>
+        <GradientText style={WelcomeStyles.gradientTitle}>Harmony</GradientText>
+        <Text style={WelcomeStyles.subtitle}>
           Connect with people who share your taste in music. Find your perfect
           music match!
         </Text>
-
         <TouchableOpacity
-          style={{
-            backgroundColor: COLORS.text,
-            padding: 15,
-            borderRadius: 15,
-            marginTop: 30,
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginHorizontal: 10,
-          }}
+          style={WelcomeStyles.joinNowButton}
           onPress={() => navigation.navigate('SignupView')}>
-          <Text
-            style={{color: COLORS.primary, fontSize: 16, fontWeight: 'medium'}}>
-            Join Now!
-          </Text>
+          <Text style={WelcomeStyles.joinNowButtonText}>Join Now!</Text>
         </TouchableOpacity>
-        <View
-          style={{
-            padding: 15,
-            marginTop: 5,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
+        <View style={WelcomeStyles.signInContainer}>
           <Pressable onPress={() => navigation.navigate('LoginView')}>
-            <Text style={{color: COLORS.text, fontSize: 15}}>
+            <Text style={WelcomeStyles.signInText}>
               Already have an account? Sign In!
             </Text>
           </Pressable>
