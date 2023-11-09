4babfa8f5bc4fbf3ee9ea9a1c679d34314a1d9c2
Yagiz Devre <hd0216@princeton.edu>
2023-11-09 02:12:47 -0500

    spotify api saved episodes

---
+++ Harmony/api/spotify/Saved/SavedEpisodes.js
@@ -0,0 +1,10 @@
+export async function TopArtistsShortTerm(code) {
+  const result = await fetch(
+    'https://api.spotify.com/v1/me/episodes?limit=50&offset=0',
+    {
+      method: 'GET',
+      headers: {Authorization: `Bearer ${code}`},
+    },
+  );
+  return await result.json();
+}
