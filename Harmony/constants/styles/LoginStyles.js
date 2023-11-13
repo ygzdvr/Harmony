50b8e5850f0f56011d9981cb350f0690ada8cdc4
Yagiz Devre <hd0216@princeton.edu>
2023-11-13 11:55:14 -0500

    some file managament

---
+++ Harmony/constants/styles/LoginStyles.js
@@ -0,0 +1,76 @@
+import {StyleSheet} from 'react-native';
+
+import COLORS from '../colors';
+
+const LoginStyles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.background,
+    flex: 1,
+    alignItems: 'flex-start',
+    paddingLeft: 20,
+  },
+  backButton: {
+    marginTop: 30,
+    padding: 7,
+    backgroundColor: COLORS.text,
+    borderRadius: 8,
+    alignSelf: 'flex-start',
+    marginBottom: 20,
+  },
+  welcomeBack: {
+    color: COLORS.text,
+    fontSize: 28,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    marginBottom: 10,
+  },
+  welcomeText: {
+    color: COLORS.text,
+    fontSize: 15,
+    alignSelf: 'flex-start',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: '100%',
+  },
+  inputLabel: {
+    color: COLORS.text,
+    marginBottom: 5,
+    marginLeft: 4,
+  },
+  textInput: {
+    backgroundColor: COLORS.text,
+    height: 50,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 20,
+    width: '95%',
+  },
+  signInButton: {
+    backgroundColor: COLORS.titlePurple,
+    padding: 15,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '95%',
+    marginBottom: 20,
+  },
+  signInText: {
+    color: COLORS.text,
+    fontSize: 16,
+    fontWeight: 'medium',
+  },
+  signUpButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingRight: 20,
+    width: '100%',
+  },
+  signUpText: {
+    color: COLORS.text,
+    fontSize: 15,
+    alignSelf: 'center',
+  },
+});
+export default LoginStyles;
--- Harmony/screens/authentication/LoginView.js
+++ Harmony/screens/authentication/LoginView.js
@@ -8,33 +8,34 @@ import {
 import React from 'react';
 import COLORS from '../../constants/colors';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import LoginStyles from '../../constants/styles/LoginStyles';
 
 const LoginView = ({navigation}) => {
   return (
-    <View style={styles.container}>
+    <View style={LoginStyles.container}>
       <TouchableOpacity
         onPress={() => navigation.goBack()}
-        style={styles.backButton}>
+        style={LoginStyles.backButton}>
         <Ionicons name="chevron-back" size={20} color={COLORS.primary} />
       </TouchableOpacity>
 
-      <Text style={styles.welcomeBack}>Welcome back!</Text>
-      <Text style={styles.welcomeText}>
+      <Text style={LoginStyles.welcomeBack}>Welcome back!</Text>
+      <Text style={LoginStyles.welcomeText}>
         It's great to see you again!Your next musical adventure awaits!
       </Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Username or Email</Text>
+      <View style={LoginStyles.inputContainer}>
+        <Text style={LoginStyles.inputLabel}>Username or Email</Text>
         <TextInput
-          style={styles.textInput}
+          style={LoginStyles.textInput}
           placeholder="Enter your username"
           placeholderTextColor={COLORS.background}
           keyboardType="email-address"
           textContentType="emailAddress"
         />
 
-        <Text style={styles.inputLabel}>Password</Text>
+        <Text style={LoginStyles.inputLabel}>Password</Text>
         <TextInput
-          style={styles.textInput}
+          style={LoginStyles.textInput}
           placeholder="Enter your password"
           placeholderTextColor={COLORS.background}
           secureTextEntry={true}
@@ -43,90 +44,20 @@ const LoginView = ({navigation}) => {
       </View>
 
       <TouchableOpacity
-        style={styles.signInButton}
+        style={LoginStyles.signInButton}
         onPress={() => navigation.navigate('HomeView')}>
-        <Text style={styles.signInText}>Sign In</Text>
+        <Text style={LoginStyles.signInText}>Sign In</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
-        style={styles.signUpButton}
+        style={LoginStyles.signUpButton}
         onPress={() => navigation.navigate('SignupView')}>
-        <Text style={styles.signUpText}>Don't have an account? Join Now!</Text>
+        <Text style={LoginStyles.signUpText}>
+          Don't have an account? Join Now!
+        </Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: COLORS.background,
-    flex: 1,
-    alignItems: 'flex-start',
-    paddingLeft: 20,
-  },
-  backButton: {
-    marginTop: 30,
-    padding: 7,
-    backgroundColor: COLORS.text,
-    borderRadius: 8,
-    alignSelf: 'flex-start',
-    marginBottom: 20,
-  },
-  welcomeBack: {
-    color: COLORS.text,
-    fontSize: 28,
-    fontWeight: 'bold',
-    alignSelf: 'flex-start',
-    marginBottom: 10,
-  },
-  welcomeText: {
-    color: COLORS.text,
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginBottom: 20,
-  },
-  inputContainer: {
-    width: '100%',
-  },
-  inputLabel: {
-    color: COLORS.text,
-    marginBottom: 5,
-    marginLeft: 4,
-  },
-  textInput: {
-    backgroundColor: COLORS.text,
-    height: 50,
-    borderRadius: 10,
-    paddingHorizontal: 15,
-    marginBottom: 20,
-    width: '95%',
-  },
-  signInButton: {
-    backgroundColor: COLORS.titlePurple,
-    padding: 15,
-    borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '95%',
-    marginBottom: 20,
-  },
-  signInText: {
-    color: COLORS.text,
-    fontSize: 16,
-    fontWeight: 'medium',
-  },
-  signUpButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    paddingRight: 20,
-    width: '100%',
-  },
-  signUpText: {
-    color: COLORS.text,
-    fontSize: 15,
-    alignSelf: 'center',
-  },
-});
 
 export default LoginView;
